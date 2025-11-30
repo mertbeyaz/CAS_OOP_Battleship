3b6550e5b36e9c27cc6d79783c1ec9c4dc4176b5
mcoppola <m.coppola@outlook.com>
2025-11-30 21:47:40 +0100

    Added some comment to Repository behavior for learning purpose.

--- src/main/java/ch/battleship/battleshipbackend/repository/GameRepository.java
+++ src/main/java/ch/battleship/battleshipbackend/repository/GameRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Spring Data JPA Repository. Schicht über JPA/Hibernate
+// Wird zur Laufzeit in GameService injected
+// und stellt Methoden wie: save, findbyId, findAll, delete, deleteById, count zur Verfügung
 public interface GameRepository extends JpaRepository<Game, UUID> {
-
     Optional<Game> findByGameCode(String gameCode);
 }
